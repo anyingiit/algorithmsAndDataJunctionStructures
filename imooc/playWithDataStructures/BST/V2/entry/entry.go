80fea6962f87d020ed64a469e2bc57941f13eac5
anyingiit <49945850+anyingiit@users.noreply.github.com>
2022-05-11 00:50:06 +0800

    add 二分搜索树: 增, 部分查: 广度优先遍历(前序遍历, 中序遍历, 后续遍历, 非递归前序遍历)

---
+++ imooc/playWithDataStructures/BST/V2/entry/entry.go
@@ -0,0 +1,37 @@
+package main
+
+import (
+	"algorithmsAndDataJunctionStructures/imooc/playWithDataStructures/BST/V2"
+	"fmt"
+)
+
+func main() {
+	bst := V2.NewBST()
+	bst.Push(41)
+	bst.Push(22)
+	bst.Push(58)
+	bst.Push(15)
+	bst.Push(33)
+	bst.Push(13)
+	bst.Push(37)
+	bst.Push(50)
+	bst.Push(42)
+	bst.Push(53)
+	bst.PreOrder()
+	fmt.Println("bst size:", bst.GetSize())
+	fmt.Println("has 50?", bst.Has(50))
+	fmt.Println("has 0?", bst.Has(0))
+
+	// 老师留的作业, 在完成二分搜索树学习后可以尝试完成
+	// 如何通过字符的方式打印易读的二叉树?
+
+	fmt.Println(bst)
+
+	bst.InOrder()
+	fmt.Println()
+
+	bst.PostOrder()
+	fmt.Println()
+
+	bst.PreOrderNR()
+}
