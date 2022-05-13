4a0149e57a23355763799de3f5f6bdea67e0d4d7
anyingiit <anyingiit@gmail.com>
2022-05-13 15:45:44 +0800

    对包OrderSlice内 添加说明

--- imooc/playWithDataStructures/OrderSlice/GetNoRepeat.go
+++ imooc/playWithDataStructures/OrderSlice/GetNoRepeat.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 )
 
+// GetNoRepeat 获取有序数组的不重复元素, 如果输入参数不具有顺序则返回错误
 func GetNoRepeat(orderedSlice []int) ([]int, error) {
 	if !IsOrder(orderedSlice) {
 		return nil, fmt.Errorf("can not get no repeat slice: input silice not is a ordered slice")
@@ -11,6 +12,9 @@ func GetNoRepeat(orderedSlice []int) ([]int, error) {
 	return GetNoRepeatNoCheck(orderedSlice), nil
 }
 
+// GetNoRepeatNoCheck 获取有序数组的不重复元素, 但是不检查参数是否具有顺序
+//
+// 请务必确保输入参数具有顺序, 否则将获得错误的返回
 func GetNoRepeatNoCheck(orderedSlice []int) []int {
 	if len(orderedSlice) == 0 || len(orderedSlice) == 1 {
 		return orderedSlice
