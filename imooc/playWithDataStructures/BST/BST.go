d80c2441d52a3edbf87f2f99e1e84141ec707530
anyingiit <49945850+anyingiit@users.noreply.github.com>
2022-05-14 21:49:36 +0800

    在原有V3 BST上进行近乎100%的重构: 1. 重命名一些不清晰的函数名称 2. 重构大量方法, 使其更为符合逻辑

--- imooc/playWithDataStructures/BST/BST.go
+++ imooc/playWithDataStructures/BST/BST.go
@@ -21,6 +21,72 @@ func newNode(e int) *node {
 	}
 }
 
+func (n *node) Minimum() (result *node) {
+	if n == nil {
+		return nil
+	}
+	if n.Left == nil {
+		return n
+	}
+	return n.Left.Minimum()
+}
+
+func (n *node) Maximum() (result *node) {
+	if n == nil {
+		return nil
+	}
+	if n.Right == nil {
+		return n
+	}
+	return n.Right.Maximum()
+}
+
+func (n *node) RemoveMin() (successor *node, deletedNode *node) {
+	if n == nil {
+		return nil, nil
+	}
+	if n.Left == nil {
+		return n.Right, n
+	}
+	n.Left, deletedNode = n.Left.RemoveMin()
+	return n, deletedNode
+}
+
+func (n *node) RemoveMax() (successor *node, deletedNode *node) {
+	if n == nil {
+		return nil, nil
+	}
+	if n.Right == nil {
+		return n.Left, n
+	}
+	n.Right, deletedNode = n.Right.RemoveMax()
+	return n, deletedNode
+}
+
+func (n *node) Remove(e int) (successor *node, deletedNode *node) {
+	if n == nil {
+		return nil, nil
+	}
+
+	if n.e > e {
+		n.Left, deletedNode = n.Left.Remove(e)
+	} else if n.e < e {
+		n.Right, deletedNode = n.Right.Remove(e)
+	} else { // targetNode.e == e
+		if n.Left == nil {
+			return n.Right, n
+		} else if n.Right == nil {
+			return n.Left, n
+		} else { // n.Left != nil && n.Right != nil
+			n.Right, successor = n.Right.RemoveMin()
+			successor.Left = n.Left
+			successor.Right = n.Right
+			return successor, n
+		}
+	}
+	return n, deletedNode
+}
+
 // BST 不重复的二分搜索树
 type BST struct {
 	root *node
@@ -42,76 +108,80 @@ func (b *BST) IsEmpty() bool {
 	return b.Size() == 0
 }
 
-func (b *BST) push(node *node, e int) *node {
-	if node == nil {
-		b.size++
-		return newNode(e)
-	}
-	if node.e > e {
-		node.Left = b.push(node.Left, e)
-	} else if node.e < e {
-		node.Right = b.push(node.Right, e)
-	}
-	return node
-}
-
 func (b *BST) Push(e int) {
-	b.root = b.push(b.root, e)
-}
+	var push func(node *node) *node
+	push = func(node *node) *node {
+		if node == nil {
+			b.size++
+			return newNode(e)
+		}
+		if node.e > e {
+			node.Left = push(node.Left)
+		} else if node.e < e {
+			node.Right = push(node.Right)
+		}
+		return node
+	}
 
-func (b *BST) has(node *node, e int) bool {
-	if node == nil {
-		return false
-	}
-	if node.e > e {
-		return b.has(node.Left, e)
-	} else if node.e < e {
-		return b.has(node.Right, e)
-	}
-	return true
+	b.root = push(b.root)
 }
 
 func (b *BST) Has(e int) bool {
-	return b.has(b.root, e)
-}
-
-func (b *BST) preOrder(node *node) {
-	if node == nil {
-		return
+	var has func(node *node) bool
+	has = func(node *node) bool {
+		if node == nil {
+			return false
+		}
+		if node.e > e {
+			return has(node.Left)
+		} else if node.e < e {
+			return has(node.Right)
+		}
+		return true
 	}
-	fmt.Println(node.e)
-	b.preOrder(node.Left)
-	b.preOrder(node.Right)
+
+	return has(b.root)
 }
 
-func (b *BST) PreOrder() {
-	b.preOrder(b.root)
-}
-
-func (b *BST) inOrder(node *node) {
-	if node == nil {
-		return
+func (b *BST) PrintPreOrder() {
+	var preOrder func(node *node)
+	preOrder = func(node *node) {
+		if node == nil {
+			return
+		}
+		fmt.Println(node.e)
+		preOrder(node.Left)
+		preOrder(node.Right)
 	}
-	b.inOrder(node.Left)
-	fmt.Println(node.e)
-	b.inOrder(node.Right)
+
+	preOrder(b.root)
 }
 
-func (b *BST) InOrder() {
-	b.inOrder(b.root)
-}
-
-func (b *BST) postOrder(node *node) {
-	if node == nil {
-		return
+func (b *BST) PrintInOrder() {
+	var inOrder func(node *node)
+	inOrder = func(node *node) {
+		if node == nil {
+			return
+		}
+		inOrder(node.Left)
+		fmt.Println(node.e)
+		inOrder(node.Right)
 	}
-	b.postOrder(node.Left)
-	b.postOrder(node.Right)
-	fmt.Println(node.e)
+	inOrder(b.root)
 }
 
-func (b *BST) PostOrder() {
-	b.postOrder(b.root)
+func (b *BST) PrintPostOrder() {
+	var postOrder func(node *node)
+	postOrder = func(node *node) {
+		if node == nil {
+			return
+		}
+		postOrder(node.Left)
+		postOrder(node.Right)
+		fmt.Println(node.e)
+	}
+
+	postOrder(b.root)
 }
 
 func (b *BST) GetPreOrderNR() (result []int) {
@@ -144,134 +214,148 @@ func (b *BST) GetLevelOrderNR() (result []int) {
 	return result
 }
 
-func (b *BST) getPreOrderFormatString(node *node, depth int) (result string) {
-	if node == nil {
-		return b.generateDepthString(depth) + "null" + "\n"
-	}
-	return b.generateDepthString(depth) + strconv.Itoa(node.e) + "\n" + b.getPreOrderFormatString(node.Left, depth+1) + b.getPreOrderFormatString(node.Right, depth+1)
-}
-
-func (b *BST) generateDepthString(depth int) (depthString string) {
-	for i := 0; i < depth; i++ {
-		depthString += "--"
-	}
-	return depthString
-}
-
 func (b *BST) GetPreOrderFormatString() (result string) {
-	return b.getPreOrderFormatString(b.root, 0)
+	generateDepthString := func(depth int) (depthString string) {
+		for i := 0; i < depth; i++ {
+			depthString += "--"
+		}
+		return depthString
+	}
+	var getPreOrderFormatString func(node *node, depth int) (result string)
+	getPreOrderFormatString = func(node *node, depth int) (result string) {
+		if node == nil {
+			return generateDepthString(depth) + "null" + "\n"
+		}
+		return generateDepthString(depth) + strconv.Itoa(node.e) + "\n" + getPreOrderFormatString(node.Left, depth+1) + getPreOrderFormatString(node.Right, depth+1)
+	}
+
+	return getPreOrderFormatString(b.root, 0)
 }
 
 func (b *BST) String() string {
 	return b.GetPreOrderFormatString()
 }
 
-func (b *BST) minimum(node *node) *node {
-	if node.Left == nil {
-		return node
-	}
-	return b.minimum(node.Left)
-}
-
-func (b *BST) Minimum() (minimum int, err error) { //TODO: 到底是返回节点还是值?
+func (b *BST) Minimum() (minimumElement int, err error) { //TODO: 到底是返回节点还是值?
 	if b.IsEmpty() {
-		return 0, fmt.Errorf("get minimum failed: the BST is empty")
+		return 0, fmt.Errorf("get Minimum failed: the BST is empty")
 	}
-	return b.minimum(b.root).e, nil
+	return b.root.Minimum().e, nil
 }
 
-func (b *BST) maximum(node *node) *node {
-	if node.Right == nil {
-		return node
-	}
-	return b.maximum(node.Right)
-}
-
-func (b *BST) Maximum() (maximum int, err error) { //TODO: 到底是返回节点还是值?
+func (b *BST) Maximum() (maximumElement int, err error) { //TODO: 到底是返回节点还是值?
 	if b.IsEmpty() {
-		return 0, fmt.Errorf("get maximum failed: the BST is empty")
+		return 0, fmt.Errorf("get Maximum failed: the BST is empty")
 	}
-	return b.maximum(b.root).e, nil
+	return b.root.Maximum().e, nil
 }
 
-func (b *BST) removeMin(node *node) *node { //TODO: logic err
-	if node.Left == nil {
-		b.size--
-		return node.Right
-	}
-	node.Left = b.removeMin(node.Left)
-	return node
-}
+//// RemoveTargetNodeMin 如果随便传入一个node, 而且对这个node执行删除min成功了, 会对本二分搜索树size--, 这不合理
+//func (b *BST) RemoveTargetNodeMin(targetNode *node) (deletedNode *node, err error) {
+//	if targetNode == nil {
+//		return nil, fmt.Errorf("cant remove target node min: target node is nil")
+//	}
+//
+//	targetNode, deletedNode = targetNode.RemoveMin()
+//
+//	if deletedNode == nil {
+//		return nil, fmt.Errorf("cant remove target node min: other err")
+//	}
+//	b.size--
+//	return targetNode, nil
+//}
+//
+//// RemoveTargetNodeMax 如果随便传入一个node, 而且对这个node执行删除min成功了, 会对本二分搜索树size--, 这不合理
+//func (b *BST) RemoveTargetNodeMax(targetNode *node) (deletedNode *node, err error) {
+//	if targetNode == nil {
+//		return nil, fmt.Errorf("cant remove target node max: target node is nil")
+//	}
+//
+//	targetNode, deletedNode = targetNode.RemoveMax()
+//
+//	if deletedNode == nil {
+//		return nil, fmt.Errorf("cant remove target node max: other err")
+//	}
+//	b.size--
+//	return targetNode, nil
+//}
 
-func (b *BST) RemoveMin() (e int, err error) {
-	minimum, err := b.Minimum()
-	if err != nil {
-		return 0, err
-	}
-	b.root = b.removeMin(b.root)
-	return minimum, nil
-}
-
-func (b *BST) removeMax(node *node) *node {
-	if node.Right == nil {
-		b.size--
-		return node.Left
-	}
-	node.Right = b.removeMax(node.Right)
-	return node
-}
-
-func (b *BST) RemoveMax() (e int, err error) {
-	maximum, err := b.Maximum()
-	if err != nil {
-		return 0, err
-	}
-	b.root = b.removeMax(b.root)
-	return maximum, nil
-}
-
-func (b *BST) remove(node *node, e int) *node { //TODO: logic error
-	if node == nil {
-		return nil
+func (b *BST) RemoveMin() (deletedNode *node, err error) {
+	if b.IsEmpty() {
+		return nil, fmt.Errorf("cant remove min: BST is empty")
 	}
 
-	if node.e > e {
-		node.Left = b.remove(node.Left, e)
-	} else if node.e < e {
-		node.Right = b.remove(node.Right, e)
-	} else { // node.e == e
-		if node.Left == nil {
-			b.size--
-			return node.Right
-		} else if node.Right == nil {
-			b.size--
-			return node.Left
-		} else {
-			successor := b.minimum(node.Right)
-			// 这里踩了个坑, 注意注意!! !!!!必须要先对右节点进行赋值!!!!, 因为当node.Right没有孩子的情况下, successor和node.Right指向的节点是相同的, 在这种情况下, 如果先对successor设置左节点, 就相当于`node.Right.Left=node.Left`, 那么当进行node.Right为根的删除操作时, 函数会错误的视为node.Left(或子树中)为node.Right子树的最小的节点作为返回, 那么返回值就是node.Left, 返回值被赋予给了successor.Right. 假设一种情况: 当前节点的左孩子是叶子节点时, 先进行`successor.Left = node.Left`, 在进行`successor.Right = b.removeMin(node.Right)`时, successor.Left的结果是正确的的, 但是successor.Right的结果一定是successor.Left, 这是错误的, 期望successor.Right为空
-			//		if node.Right.Left == nil && node.Right.Left == nil{
-			//			successor.Left = node.Left
-			//			successor.Right = b.removeMin(node.Right)
-			//			// [actually]
-			//			//		successor.Left == node.Left
-			//			//		successor.Right == node.Left
-			//			// [expect]
-			//			//		success.Left == node.Left
-			//			//		success.Right == nil
-			//		}
-			successor.Right = b.removeMin(node.Right) // removeMin 本身会进行size--, 所以不需要进行size--了
-			successor.Left = node.Left
-			return successor
-		}
+	b.root, deletedNode = b.root.RemoveMin()
+
+	if deletedNode == nil {
+		return nil, fmt.Errorf("cannot remove min: other error")
 	}
-	return node
+	b.size--
+	return deletedNode, nil
 }
+
+func (b *BST) RemoveMax() (deletedNode *node, err error) {
+	if b.IsEmpty() {
+		return nil, fmt.Errorf("cant remove min: BST is empty")
+	}
+
+	b.root, deletedNode = b.root.RemoveMax()
+
+	if deletedNode == nil {
+		return nil, fmt.Errorf("cannot remove max: other error")
+	}
+	b.size--
+	return deletedNode, nil
+}
+
+//func (b *BST) remove(targetNode *node, e int) *node { //TODO: logic error
+//	if targetNode == nil {
+//		return nil
+//	}
+//
+//	if targetNode.e > e {
+//		targetNode.Left = b.remove(targetNode.Left, e)
+//	} else if targetNode.e < e {
+//		targetNode.Right = b.remove(targetNode.Right, e)
+//	} else { // targetNode.e == e
+//		if targetNode.Left == nil {
+//			b.size--
+//			return targetNode.Right
+//		} else if targetNode.Right == nil {
+//			b.size--
+//			return targetNode.Left
+//		} else {
+//			var successor *node
+//			targetNode.Right, successor = targetNode.Right.RemoveMin()
+//			b.size--
+//			// 这里踩了个坑, 注意注意!! !!!!必须要先对右节点进行赋值!!!!, 因为当node.Right没有孩子的情况下, successor和node.Right指向的节点是相同的, 在这种情况下, 如果先对successor设置左节点, 就相当于`targetNode.Right.Left=targetNode.Left`, 那么当进行node.Right为根的删除操作时, 函数会错误的视为node.Left(或子树中)为node.Right子树的最小的节点作为返回, 那么返回值就是node.Left, 返回值被赋予给了successor.Right. 假设一种情况: 当前节点的左孩子是叶子节点时, 先进行`successor.Left = targetNode.Left`, 在进行`successor.Right = b.RemoveMin(targetNode.Right)`时, successor.Left的结果是正确的的, 但是successor.Right的结果一定是successor.Left, 这是错误的, 期望successor.Right为空
+//			//		if targetNode.Right.Left == nil && targetNode.Right.Left == nil{
+//			//			successor.Left = targetNode.Left
+//			//			successor.Right = b.RemoveMin(targetNode.Right)
+//			//			// [actually]
+//			//			//		successor.Left == targetNode.Left
+//			//			//		successor.Right == targetNode.Left
+//			//			// [expect]
+//			//			//		success.Left == targetNode.Left
+//			//			//		success.Right == nil
+//			//		}
+//			successor.Right = targetNode.Right
+//			successor.Left = targetNode.Left
+//			return successor
+//		}
+//	}
+//	return targetNode
+//}
 
 func (b *BST) Remove(e int) error { //TODO: logic error
-	originSize := b.Size()
-	b.root = b.remove(b.root, e)
-	if b.Size() != originSize-1 {
-		return fmt.Errorf("failed remove %d", e)
+	var deletedNode *node
+	b.root, deletedNode = b.root.Remove(e)
+
+	if deletedNode == nil {
+		return fmt.Errorf("cannot remove element %d: cant find this element", e)
 	}
+
+	b.size--
+
 	return nil
 }
--- imooc/playWithDataStructures/BST/BST_test.go
+++ imooc/playWithDataStructures/BST/BST_test.go
@@ -86,10 +86,10 @@ func TestBST_RemoveMin(t *testing.T) {
 	}
 
 	tests := []test{
-		{
-			[]int{5},
-			[]int{5},
-		},
+		//{
+		//	[]int{5},
+		//	[]int{5},
+		//},
 		{
 			[]int{5, 8, 6, 1, 4},
 			[]int{1, 4, 5, 6, 8},
@@ -147,7 +147,7 @@ func TestBST_RemoveMin(t *testing.T) {
 			if err != nil {
 				t.Errorf("test BST remove min failed: excute RemoveMin has error %s", err.Error())
 			}
-			actually = append(actually, min)
+			actually = append(actually, min.e)
 		}
 		if len(actually) != len(tt.expect) {
 			t.Errorf("test BST remove min failed: actually delete element count check failed, actually %d expect %d", len(actually), len(tt.expect))
@@ -228,7 +228,7 @@ func TestBST_RemoveMax(t *testing.T) {
 			if err != nil {
 				t.Errorf("test BST remove max failed: excute RemoveMax has error %s", err.Error())
 			}
-			actually = append(actually, max)
+			actually = append(actually, max.e)
 		}
 		if len(actually) != len(tt.expect) {
 			t.Errorf("test BST remove max failed: actually delete element count check failed, actually %d expect %d", len(actually), len(tt.expect))
@@ -318,7 +318,7 @@ func TestBST_Remove(t *testing.T) {
 		for _, e := range tt.deleteTarget {
 			err := bst.Remove(e)
 			if err != nil {
-				t.Errorf("test BST remove element failed: %s", err.Error())
+				t.Errorf("test BST remove element failed: %s, input %v deleteTarget %v expect %v", err.Error(), tt.input, tt.deleteTarget, tt.expect)
 			}
 		}
 
@@ -329,7 +329,7 @@ func TestBST_Remove(t *testing.T) {
 			if err != nil {
 				t.Errorf("test BST remove failed: BST not is empty, but excute RemoveMax has error %s", err.Error())
 			}
-			actually = append(actually, min)
+			actually = append(actually, min.e)
 		}
 		if len(actually) != len(tt.expect) {
 			t.Errorf("test BST remove failed: actually delete element count check failed, actually %d expect %d", len(actually), len(tt.expect))
