a2aa4fe2b8c2b846433e4dcb9ba6fa0d841560f2
anyingiit <49945850+anyingiit@users.noreply.github.com>
2022-05-15 11:38:24 +0800

    rename: SortByDESC, SortByASC -> SetSortByDESC, SetSortByASC add: GetSortByDESC, GetSortByASC and then test, example

--- imooc/playWithDataStructures/BubbleSort/BubbleSort_test.go
+++ imooc/playWithDataStructures/BubbleSort/BubbleSort_test.go
@@ -9,7 +9,7 @@ import (
 //
 // 1. 排序后长度测试
 // 2. 排序后顺序测试
-func TestSortByDESC(t *testing.T) {
+func TestSetSortByDESC(t *testing.T) {
 	var tests []int
 	for i := 0; i < 1000; i++ {
 		tests = append(tests, rand.Intn(10000))
@@ -17,7 +17,7 @@ func TestSortByDESC(t *testing.T) {
 
 	originLen := len(tests)
 
-	SortByDESC(tests)
+	SetSortByDESC(tests)
 
 	if len(tests) != originLen {
 		t.Errorf("len test failed expect %d, actually %d", originLen, len(tests))
@@ -34,7 +34,7 @@ func TestSortByDESC(t *testing.T) {
 //
 // 1. 排序后长度测试
 // 2. 排序后顺序测试
-func TestSortByASC(t *testing.T) {
+func TestSetSortByASC(t *testing.T) {
 	var tests []int
 	for i := 0; i < 1000; i++ {
 		tests = append(tests, rand.Intn(10000))
@@ -42,7 +42,7 @@ func TestSortByASC(t *testing.T) {
 
 	originLen := len(tests)
 
-	SortByASC(tests)
+	SetSortByASC(tests)
 
 	if len(tests) != originLen {
 		t.Errorf("len test failed expect %d, actually %d", originLen, len(tests))
@@ -54,4 +54,64 @@ func TestSortByASC(t *testing.T) {
 		}
 	}
 
+}
+
+func TestGetSortByDESC(t *testing.T) {
+	var tests []int
+	for i := 0; i < 1000; i++ {
+		tests = append(tests, rand.Intn(10000))
+	}
+
+	testsCopy := make([]int, len(tests))
+	copy(testsCopy, tests)
+
+	descResult := GetSortByDESC(tests)
+
+	if len(tests) != len(testsCopy) {
+		t.Errorf("check GetSortByDESC is changed origin slice failed, originLen %d actuallyLen %d. origin %v actully %v", len(testsCopy), len(tests), testsCopy, tests)
+	}
+	for i, e := range testsCopy {
+		if tests[i] != e {
+			t.Errorf("check GetSortByDESC is changed origin slice failed, actually %v expect %v. tests %v testsCopy %v", tests[i], e, tests, testsCopy)
+		}
+	}
+
+	if len(descResult) != len(tests) {
+		t.Errorf("len test failed expect %d, actually %d", len(tests), len(descResult))
+	}
+	for i := 0; i < len(descResult)-1; i++ {
+		if descResult[i] > descResult[i+1] {
+			t.Errorf("test failed")
+		}
+	}
+}
+
+func TestGetSortByASC(t *testing.T) {
+	var tests []int
+	for i := 0; i < 1000; i++ {
+		tests = append(tests, rand.Intn(10000))
+	}
+
+	testsCopy := make([]int, len(tests))
+	copy(testsCopy, tests)
+
+	ascResult := GetSortByASC(tests)
+
+	if len(tests) != len(testsCopy) {
+		t.Errorf("check GetSortByDESC is changed origin slice failed, originLen %d actuallyLen %d. origin %v actully %v", len(testsCopy), len(tests), testsCopy, tests)
+	}
+	for i, e := range testsCopy {
+		if tests[i] != e {
+			t.Errorf("check GetSortByDESC is changed origin slice failed, actually %v expect %v. tests %v testsCopy %v", tests[i], e, tests, testsCopy)
+		}
+	}
+
+	if len(ascResult) != len(tests) {
+		t.Errorf("len test failed expect %d, actually %d", len(tests), len(ascResult))
+	}
+	for i := 0; i < len(ascResult)-1; i++ {
+		if ascResult[i] < ascResult[i+1] {
+			t.Errorf("test failed")
+		}
+	}
 }
