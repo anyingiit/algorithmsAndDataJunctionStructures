163077ce4f07ccfc8776aa3c8ac9846921e728e9
anyingiit <49945850+anyingiit@users.noreply.github.com>
2022-05-24 03:29:02 +0800

    for MaxHeap add Replace, Heapify

--- imooc/playWithDataStructures/MaxHeap/V1/MaxHeap.go
+++ imooc/playWithDataStructures/MaxHeap/V1/MaxHeap.go
@@ -13,6 +13,38 @@ func NewMaxHeap() *MaxHeap {
 	return &MaxHeap{nil}
 }
 
+// NewMaxHeapForArr Heapify
+func NewMaxHeapForArr(e []int) *MaxHeap {
+	maxHeap := NewMaxHeap()
+	maxHeap.data = make([]int, len(e))
+	copy(maxHeap.data, e)
+	// Heapify 将乱序的数组转换为一个二叉堆
+	// * 也可以创建一个新的maxHeap然后使用Add向其添加元素, 但是那样没有这样快
+	// 为什么不从最后一个元素使用siftUp直接遍历完成呢?
+	//		因为siftUp会假设当前元素上边的元素顺序是正确的
+	//		而现在的情况是 顺序是错误的
+	// 为什么不从第一个元素使用siftDown直到遍历完成?
+	//		因为siftDown会假设当前元素下面的元素是正确的的
+	//		而现在的实际情况是 顺序是错误的
+	// 根据以上两点, 正序siftDown不行, 倒序siftUp不行, 倒序使用siftDown行, 那么正序使用siftUp行不行呢?
+	//		经过实际验证, 是可行的, 但是应该效率没倒序siftDown高
+	//cpu: Intel(R) Core(TM) i7-4790K CPU @ 4.00GHz
+	//BenchmarkNewMaxHeapForArr
+	//BenchmarkNewMaxHeapForArr-8        98644             13001 ns/op
+	//PASS
+	for i := maxHeap.Size() - 1; i >= 0; i-- {
+		maxHeap.siftDown(i)
+	}
+	//cpu: Intel(R) Core(TM) i7-4790K CPU @ 4.00GHz
+	//BenchmarkNewMaxHeapForArr
+	//BenchmarkNewMaxHeapForArr-8        80864             14386 ns/op
+	//PASS
+	//for i := 0; i < maxHeap.Size()-1; i++ {
+	//	maxHeap.siftUp(i)
+	//}
+	return maxHeap
+}
+
 func (m *MaxHeap) Size() int {
 	return len(m.data)
 }
@@ -53,14 +85,15 @@ func (m *MaxHeap) siftUp(index int) {
 
 // ExtractMax 提取最大元素
 func (m *MaxHeap) ExtractMax() (int, error) {
-	if m.IsEmpty() {
-		return 0, fmt.Errorf("cannot extract max, beacuse the Max Heap is empty")
+	maxTemp, err := m.FindMax()
+	if err != nil {
+		return 0, fmt.Errorf("cannot extract max: %s", err.Error())
 	}
-	max := m.data[0]
+
 	m.data[0] = m.data[m.Size()-1]
 	m.data = m.data[:m.Size()-1]
 	m.siftDown(0)
-	return max, nil
+	return maxTemp, nil
 }
 
 // siftDown 使用非递归下浮(siftDown)
@@ -86,4 +119,22 @@ func (m *MaxHeap) hasIndex(index int) bool {
 		return false
 	}
 	return true
+}
+
+func (m *MaxHeap) FindMax() (int, error) {
+	if m.IsEmpty() {
+		return 0, fmt.Errorf("cannot find max element: the MaxHeap is empty")
+	}
+	return m.data[0], nil
+}
+
+// Replace 提取最大元素并添加一个新元素
+func (m *MaxHeap) Replace(e int) (int, error) {
+	maxTemp, err := m.FindMax()
+	if err != nil {
+		return 0, fmt.Errorf("cannot Replace: %s", err.Error())
+	}
+	m.data[0] = e
+	m.siftDown(0)
+	return maxTemp, nil
 }
