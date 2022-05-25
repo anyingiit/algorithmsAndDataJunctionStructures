a20e5c05a2694d72930ffe9daa2d43b96d4cf50a
anyingiit <49945850+anyingiit@users.noreply.github.com>
2022-05-25 15:10:22 +0800

    for MaxHeap add Interface

--- imooc/playWithDataStructures/MaxHeap/MaxHeap.go
+++ imooc/playWithDataStructures/MaxHeap/MaxHeap.go
@@ -1,1 +1,29 @@
 package MaxHeap
+
+import "algorithmsAndDataJunctionStructures/imooc/playWithDataStructures/MaxHeap/V1"
+
+type MaxHeap interface {
+	// Size return the MaxHeap size
+	Size() int
+	// IsEmpty if the MaxHeap is empty then return true else return false
+	IsEmpty() bool
+	// Add for MaxHeap add any element
+	Add(e int)
+	// ExtractMax pop max element
+	// if the MaxHeap is empty then return err
+	ExtractMax() (int, error)
+	// FindMax return max element
+	// if the MaxHeap is empty then return err
+	FindMax() (int, error)
+	// Replace pop max element and add an element
+	// if the MaxHeap is empty then return err
+	Replace(e int) (int, error)
+}
+
+func NewMaxHeap() MaxHeap {
+	return V1.NewMaxHeap()
+}
+
+func NewMaxHeapForArr(x []int) MaxHeap {
+	return V1.NewMaxHeapForArr(x)
+}
