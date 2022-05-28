bd542bb194977ff5713226cddd19fda146c0cf6c
anyingiit <49945850+anyingiit@users.noreply.github.com>
2022-05-28 12:01:32 +0800

    add UnionFindV4 for quick union 基于rank and Path Compression的优化

--- imooc/playWithDataStructures/UnionFind/UF_test.go
+++ imooc/playWithDataStructures/UnionFind/UF_test.go
@@ -5,6 +5,7 @@ import (
 	"algorithmsAndDataJunctionStructures/imooc/playWithDataStructures/UnionFind/QuickUnionV1"
 	"algorithmsAndDataJunctionStructures/imooc/playWithDataStructures/UnionFind/QuickUnionV2"
 	"algorithmsAndDataJunctionStructures/imooc/playWithDataStructures/UnionFind/QuickUnionV3"
+	"algorithmsAndDataJunctionStructures/imooc/playWithDataStructures/UnionFind/QuickUnionV4"
 	"math/rand"
 	"testing"
 )
@@ -83,7 +84,7 @@ func BenchmarkQuickUnionV3(b *testing.B) {
 
 func BenchmarkQuickUnionV4(b *testing.B) {
 	for i := 0; i < b.N; i++ {
-		unionFind := QuickUnionV3.NewUnionFind(b.N)
+		unionFind := QuickUnionV4.NewUnionFind(b.N)
 		for j := 0; j < b.N; j++ {
 			err := unionFind.UnionElements(rand.Intn(b.N), rand.Intn(b.N))
 			if err != nil {
